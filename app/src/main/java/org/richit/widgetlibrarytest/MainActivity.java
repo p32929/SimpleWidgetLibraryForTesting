8f8487ecbdfc053d2aabf5a1ea269cdd0d577be7
p32929 <p32929@yahoo.com>
2020-05-09 13:42:22 +0600

    Initial commit

---
+++ app/src/main/java/org/richit/widgetlibrarytest/MainActivity.java
@@ -0,0 +1,16 @@
+package org.richit.widgetlibrarytest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.richit.appwidgetlibrary.MyAppWidget1;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
