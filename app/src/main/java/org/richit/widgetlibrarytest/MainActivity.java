70d629d4d442fe01a503199fccfd2f85d6a90f87
p32929 <p32929@yahoo.com>
2020-05-09 17:05:37 +0600

    add widget to home

--- app/src/main/java/org/richit/widgetlibrarytest/MainActivity.java
+++ app/src/main/java/org/richit/widgetlibrarytest/MainActivity.java
@@ -1,8 +1,17 @@
 package org.richit.widgetlibrarytest;
 
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProviderInfo;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import org.richit.appwidgetlibrary.WidgetRelated.MyAppWidget1;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
+//        int appWidgetIds[] = appWidgetManager.getAppWidgetIds(
+//                new ComponentName(this, MyAppWidget1.class));
+//        Intent pickIntent = new Intent(AppWidgetManager.ACTION_APPWIDGET_PICK);
+//        pickIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetIds);
+//        startActivityForResult(pickIntent, 123);
+
+        addAppWidget(new Intent());
+    }
+
+    void addAppWidget(Intent data) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            AppWidgetManager mAppWidgetManager = getSystemService(AppWidgetManager.class);
+
+            ComponentName myProvider = new ComponentName(MainActivity.this,
+                    MyAppWidget1.class);
+
+            Bundle b = new Bundle();
+            b.putString("ggg", "ggg");
+            if (mAppWidgetManager.isRequestPinAppWidgetSupported()) {
+                Intent pinnedWidgetCallbackIntent = new Intent(MainActivity.this,
+                        MyAppWidget1.class);
+                PendingIntent successCallback =
+                        PendingIntent.getBroadcast(MainActivity.this, 0,
+                                pinnedWidgetCallbackIntent, 0);
+
+                mAppWidgetManager.requestPinAppWidget(myProvider, b, successCallback);
+            }
+        }
     }
 }
