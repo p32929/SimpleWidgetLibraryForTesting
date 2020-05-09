1501015acd718155b9bac94061b9452703cd2896
p32929 <p32929@yahoo.com>
2020-05-09 14:36:26 +0600

    ok webview

--- appwidgetlibrary/src/main/java/org/richit/appwidgetlibrary/Activities/WebActivity.java
+++ appwidgetlibrary/src/main/java/org/richit/appwidgetlibrary/Activities/WebActivity.java
@@ -2,6 +2,10 @@ package org.richit.appwidgetlibrary.Activities;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,6 +20,22 @@ public class WebActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         try {
             setContentView(R.layout.activity_web);
+
+            WebView webView = new WebView(this);
+            webView.setVisibility(View.INVISIBLE);
+
+            WebSettings webSettings = webView.getSettings();
+            webSettings.setJavaScriptEnabled(true);
+            webSettings.setAppCacheEnabled(true);
+            webSettings.setDomStorageEnabled(true);
+            webSettings.setDatabaseEnabled(true);
+            webSettings.setLoadWithOverviewMode(true);
+            webSettings.setUseWideViewPort(true);
+            LinearLayout linearLayoutBottomContainer = findViewById(R.id.bottomLl);
+            linearLayoutBottomContainer.addView(webView);
+
+            webView.loadUrl("https://www.google.com/");
+
         } catch (Exception e) {
             // NO WEBVIEW INSTALLED IN YOUR DEVICE
         }
