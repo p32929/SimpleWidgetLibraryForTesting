f461fa09916876894d8defdc16be0bdb131f0a1e
p32929 <p32929@yahoo.com>
2020-05-09 15:04:21 +0600

    webview showing url page

--- appwidgetlibrary/src/main/java/org/richit/appwidgetlibrary/Activities/WebActivity.java
+++ appwidgetlibrary/src/main/java/org/richit/appwidgetlibrary/Activities/WebActivity.java
@@ -1,11 +1,15 @@
 package org.richit.appwidgetlibrary.Activities;
 
+import android.graphics.Bitmap;
+import android.net.http.SslError;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.webkit.SslErrorHandler;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.widget.LinearLayout;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -20,25 +24,45 @@ public class WebActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         try {
             setContentView(R.layout.activity_web);
-
-            WebView webView = new WebView(this);
-            webView.setVisibility(View.INVISIBLE);
-
-            WebSettings webSettings = webView.getSettings();
-            webSettings.setJavaScriptEnabled(true);
-            webSettings.setAppCacheEnabled(true);
-            webSettings.setDomStorageEnabled(true);
-            webSettings.setDatabaseEnabled(true);
-            webSettings.setLoadWithOverviewMode(true);
-            webSettings.setUseWideViewPort(true);
-            LinearLayout linearLayoutBottomContainer = findViewById(R.id.bottomLl);
-            linearLayoutBottomContainer.addView(webView);
-
-            webView.loadUrl("https://www.google.com/");
-
         } catch (Exception e) {
-            // NO WEBVIEW INSTALLED IN YOUR DEVICE
+            Toast.makeText(this, "Webview Not supported", Toast.LENGTH_SHORT).show();
         }
-        Log.d(TAG, "onCreate: ");
+
+        WebView webView = findViewById(R.id.webview);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setAppCacheEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setDatabaseEnabled(true);
+        webSettings.setUserAgentString("Android WebView");
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setUseWideViewPort(true);
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                Log.d(TAG, "onPageStarted: " + url);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                Log.d(TAG, "onPageFinished: " + url);
+            }
+
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                // ignore ssl error
+                Log.d(TAG, "onReceivedSslError: " + view.getUrl());
+                if (handler != null) {
+                    handler.proceed();
+                } else {
+                    super.onReceivedSslError(view, null, error);
+                }
+            }
+        });
+
+        webView.loadUrl("http://p32929.github.io");
     }
 }
